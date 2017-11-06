4bb09785548b3dff5810e02e7ac0c4cdeacb657f
khadija <email": null>
2017-11-06 18:20:59 -0500

    some more work for logging in

--- recipe-proj/src/components/pages/LoginPage.js
+++ recipe-proj/src/components/pages/LoginPage.js
@@ -1,24 +1,32 @@
 import React from "react";
-// import PropTypes from "prop-types";
-// import { connect } from "react-redux";
-// import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import LoginForm from "../forms/LoginForm";
-// import { login } from "../../actions/auth";
-
+import { login } from "../../actions/auth";
 
 class LoginPage extends React.Component {
-  submit = data => {
-    console.log(data);
-  };
+  submit = data =>
+    this.props.login(data).then(() => this.props.history.push("/dashboard"));
 
   render() {
-    return(
-    <div>
-      <h1>Login Page</h1>
-      <LoginForm submit={this.submit}/>
-    </div>
+    return (
+      <div>
+        <h1>Login page</h1>
+
+        <LoginForm submit={this.submit} />
+
+        <Link to="/forgot_password">Forgot Password?</Link>
+      </div>
     );
   }
 }
 
-export default LoginPage;
+LoginPage.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired
+  }).isRequired,
+  login: PropTypes.func.isRequired
+};
+
+export default connect(null, { login })(LoginPage);
---
+++ recipe-proj/src/rootReducer.js
@@ -0,0 +1,13 @@
+import { combineReducers } from "redux";
+
+// import user from "./reducers/user";
+// import recipes from "./reducers/recipes";
+// import categories from "./reducers/categories";
+
+export default combineReducers({
+  user: () => ({})
+  // user,
+  // recipes,
+  // categories
+
+});
