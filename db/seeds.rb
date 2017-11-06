293d60a3764cfbdb2b4769d31c2bd9d3575c5f94
Andreas <landgrebea@allegheny.edu>
2017-11-06 12:08:31 -0500

    create seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+User.destroy_all
+Category.destroy_all
+Recipe.destroy_all
+Comment.destroy_all
+Rating.destroy_all
+
+5.times do |number|
+  User.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: "#{number}@email.com", password: "password")
+end
+
+@appetizer = Category.create(name:'Appetizer')
+@salad = Category.create(name:'Salad')
+@main_course = Category.create(name:'Main Course')
+@dessert = Category.create(name:'Dessert')
+
+15.times do
+  Recipe.create(name: Faker::Food.dish, ingredients: Faker::Food.ingredient, directions: "Please read manual", difficulty: Faker::Number.between(1, 10), prep_time: Faker::Number.digit, category_id: categories.sample, user_id: users.sample)
+end
+
+15.times do
+  Comment.create(text: "Excellent recipe", recipe_id: recipes.sample, used_id: users.sample)
+end
+
+15.times do
+  Rating.create(votes: Faker::Number.digit, recipe_id: recipes.sample, user_id: users.sample)
+end
