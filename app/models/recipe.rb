6240bd36250e60e6b6796a079d062a1d138599d2
Andreas Bach Landgrebe <andreaslandgrebe@gmail.com>
2017-11-07 18:47:31 -0500

    fix up seed file a little bit, configure routes to be nested between the recipes and the categories, and add to recipes controller to find_by specific id of recipes and comments

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,7 @@ class Recipe < ApplicationRecord
 
   belongs_to :category
   belongs_to :user
+
+  has_many :comments
+  has_many :ratings
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
-  resources :recipes, only: [:show, :new, :create]
-  resources :categories, only: [:index, :show]
+    resources :categories, only: [:index, :show] do
+      resources :recipes, only: [:show, :new, :create]
+    end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
